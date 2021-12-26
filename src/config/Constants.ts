df7912e52271e04360ffcc93e7d121ec09ffa4b4
Léo Albergaria <63436406+leo-albergaria@users.noreply.github.com>
2021-12-26 21:25:58 +0000

    Add files via upload

---
+++ src/config/Constants.ts
@@ -0,0 +1,7 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		// 'URl de Coneção',
+		"mongodb+srv://<username>:<password>@learnclustermain.ho72i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+
+}
